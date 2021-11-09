e2d1733d08c25966835255b0d08d3a9aefe6dc5f
toxidxd <toxid13@yandex.ru>
2021-11-09 12:57:38 +0300

    Def for face compare done

--- detect_by_photo.py
+++ detect_by_photo.py
@@ -1,12 +1,6 @@
-# TODO создание датасета по фото и дальнейшее распознавание
-
-"""
-1. Создание датасета по фотографиям
-2. Перебор фотографий и копирование
-3. Сохранить результаты в json
-"""
 import os
 import face_recognition
+import shutil
 
 
 def dataset_create():
@@ -15,17 +9,48 @@ def dataset_create():
     images = os.listdir("dataset_photo")
 
     for (i, img) in enumerate(images):
-        print(f"[+] Processing {i+1}/{len(images)} photo...")
+        print(f"+ Processing {i+1}/{len(images)} photo...")
         face_img = face_recognition.load_image_file(f"dataset_photo/{img}")
-        face_enc = face_recognition.face_encodings(face_img)[0]
-        enc_dataset.append(face_enc)
+        face_enc = face_recognition.face_encodings(face_img)
+
+        if len(face_enc) > 0:
+            enc_dataset.append(face_enc[0])
+        else:
+            print("++ No faces on photo!")
 
     print(f"Dataset crated from {len(enc_dataset)} photos")
     return enc_dataset
 
 
 def face_rec(dataset):
-    pass
+
+    if os.path.exists("dataset_photo"):
+        print("Directory dataset_photo exists")
+    else:
+        os.mkdir("dataset_photo")
+        print("Directory dataset_photo created")
+
+    photos = os.listdir("photos")
+    print(f"Found {len(photos)} photos to recognize\n-------")
+
+    for (i, img) in enumerate(photos):
+        print(f"+ Processing {i + 1}/{len(photos)} photo...")
+        face_img = face_recognition.load_image_file(f"photos/{img}")
+        face_enc = face_recognition.face_encodings(face_img)
+        print(f"++ Found {len(face_enc)} face on photo")
+
+        if len(face_enc) > 0:
+            for (j, face) in enumerate(face_enc):
+                print(f"+++ Processing {j+ 1}/{len(face_enc)} faces...")
+                for enc_data in dataset:
+                    compare = face_recognition.compare_faces([enc_data], face)
+                    if compare[0]:
+                        print(f"++++ We have a match in {img}")
+                        shutil.copy2(f"photos/{img}", "recognized_photos/")
+                        break
+
+        else:
+            print("+++ No faces on photo!")
 
 
 def main():
